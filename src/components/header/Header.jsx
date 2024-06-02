9c4282a5eccfe77e0f2f9f7b97f2fee82abc1fd0
muthu0138 <mk0330158@gmail.com>
2024-06-02 17:16:59 +0530

    sidebar has been removed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className="headerTitleSm">React & Node</span>
-            <span className="headerTitleLg">Blog</span>
+            <span className="headerTitle">Blog</span>
         </div>
         <img className="headerImg" src={Hey} alt="hey friend" />
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,14 @@
 import './Home.css'
 import Header from "../../components/header/Header.jsx";
 import Posts from '../../components/posts/Posts.jsx';
-import SideBar from '../../components/sidebar/SideBar.jsx';
-
 export default function Home() {
   return (
     <>
     <Header/>
+    <hr />
+    <h2>Posts</h2>
     <div className='home'>  
         <Posts/>
-        <SideBar/>
     </div>
    </>
   )
