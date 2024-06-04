6067a71c73edcced7f548e8a2aa89cbc959ca3f1
muthu0138 <mk0330158@gmail.com>
2024-06-04 10:13:02 +0530

    random changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from "./pages/home/Home.jsx";
 import Topbar from "../src/components/topbar/topbar.jsx";
 import { Route, Routes } from "react-router-dom";
+import Write from "../src/pages/write/Write.jsx"
 
 function App() {
   return (
     <>
       <Topbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Write/>} />
       </Routes>
     </>
   );
