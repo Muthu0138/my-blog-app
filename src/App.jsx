77401b569b0ad53ff93ce4e5fea12be7e657b50c
muthu0138 <mk0330158@gmail.com>
2024-06-04 09:55:54 +0530

    summa branch has been merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Home from "./pages/home/Home.jsx";
-import Topbar from "../src/components/topbar/topbar.jsx"
+import Topbar from "../src/components/topbar/topbar.jsx";
+import { Route, Routes } from "react-router-dom";
 
-
-function App(){
-
+function App() {
   return (
     <>
-      <Topbar/>
-      <Home/>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
-};
+}
 
 export default App;
